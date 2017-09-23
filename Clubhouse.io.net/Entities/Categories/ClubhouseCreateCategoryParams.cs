6765f13b887699623b85adf746ff8f0e84361901
Martyn Jones <martyn.n.jones@gmail.com>
2017-09-23 21:56:52 +0100

    Adding implementation for Category client methods
    Includes fixing some issues with the nullability and serialization of the entity types to work with the actual API.

--- Clubhouse.io.net/Entities/Categories/ClubhouseCreateCategoryParams.cs
+++ Clubhouse.io.net/Entities/Categories/ClubhouseCreateCategoryParams.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Clubhouse.io.net.Entities.Categories
 {
@@ -10,16 +11,17 @@ namespace Clubhouse.io.net.Entities.Categories
             Type = type;
         }
 
-        [JsonProperty(PropertyName = "color")]
+        [JsonProperty(PropertyName = "color", NullValueHandling = NullValueHandling.Ignore)]
         public string Color { get; set; }
 
-        [JsonProperty(PropertyName = "external_id")]
+        [JsonProperty(PropertyName = "external_id", NullValueHandling = NullValueHandling.Ignore)]
         public string ExternalID { get; set; }
 
         [JsonProperty(PropertyName = "name", Required = Required.Always)]
         public string Name { get; set; }
 
         [JsonProperty(PropertyName = "type", Required = Required.Always)]
+        [JsonConverter(typeof(StringEnumConverter))]
         public ClubhouseCategoryTypes Type { get; set; }
     }
 }
